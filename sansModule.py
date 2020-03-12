40ad70caa4a7106f37088bfcfb2752af98db2515
JarcXenon <54362249+JarcXenon@users.noreply.github.com>
2020-03-12 15:23:30 +0100

    Add files via upload

---
+++ sansModule.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+n = 45.7536
+s = 45.7305
+e = 4.8937
+w = 4.8469
+
+
+bbox = (s,w,n,e)
+condition = ["historic"]
+
+overpass_url = "http://overpass-api.de/api/interpreter"
+overpass_query = """[out:json];
+nwr{}{};
+out;
+""".format(condition, bbox)
+
+print(overpass_query)
+
+response = requests.get(overpass_url, 
+                        params={'data': overpass_query})
+data = response.json()
